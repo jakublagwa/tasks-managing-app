18a1bdca2d03fe6a68f1185aa3f0fc11a02cf0d4
Jakub Łągwa <kklagwa@gmail.com>
2023-01-26 16:56:42 +0100

    added classes connected with app model

---
+++ tasks-managing-app/Src/Model/Task.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+public class Task
+{
+    private string name;
+    private string description;
+    private DateTime startDate;
+    private DateTime dueDate;
+    private bool isCompleted;
+    private Priority priority;
+
+    public void SetName(string name)
+    {
+        this.name = name;
+    }
+    
+    public void SetDescription(string description)
+    {
+        this.description = description;
+    }
+
+    public void SetStartDate(DateTime startDate)
+    {
+        this.startDate = startDate;
+    }
+
+    public void SetDueDate(DateTime dueDate)
+    {
+        this.dueDate = dueDate;
+    }
+
+    public void Complete()
+    {
+        this.isCompleted = true;
+    }
+
+    public void SetPriority(Priority priority)
+    {
+        this.priority = priority;  
+    }
+    public bool IsCompleted()
+    {
+        return this.isCompleted;
+    }
+
+    public string GetName()
+    {
+        return this.name;
+    }
+
+    public string GetDescription()
+    {
+        return this.description;
+    }
+
+    public DateTime GetDueDate()
+    {
+        return this.dueDate;
+    }
+
+    public DateTime GetStartDate()
+    {
+        return this.startDate;
+    }
+
+    public Priority GetPriority()
+    {
+        return priority;
+    }
+}   
---
+++ tasks-managing-app/Src/Model/TaskList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class TaskList
+{
+    private List<Task> tasks;
+
+    public TaskList()
+    {
+        tasks = new List<Task>();
+    }
+
+    public void AddTask(Task task)
+    {
+        tasks.Add(task);
+    }
+
+    public void RemoveTask(Task task)
+    {
+        tasks.Remove(task);
+    }
+
+    public void EditTask(Task task, string newName, string newDescription, Priority newPriority, DateTime newDueTime, DateTime newStartTime)
+    {
+        task.SetName(newName);
+        task.SetDescription(newDescription);
+        task.SetPriority(newPriority);
+        task.SetDueDate(newDueTime);
+        task.SetStartDate(newStartTime);
+    }
+
+    public List<Task> GetAllTasks()
+    {
+        return tasks;
+    }
+
+    //FIXME
+    public List<Task> GetTasksByPriority(Priority priority)
+    {
+        return tasks.Where(t => t.GetPriority == priority).ToList();
+    }
+}
