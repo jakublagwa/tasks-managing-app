278871fde344205fd85b2b485ebfe995a2d936c5
Jakub Łągwa <kklagwa@gmail.com>
2023-01-27 15:44:37 +0100

    starting learning and implementing database connection to a project

---
+++ tasks-managing-app/Src/Data/DatabaseHandler.cs
@@ -0,0 +1,39 @@
+﻿using Java.IO;
+using MySql.Data.MySqlClient;
+
+class DatabaseHandler
+{
+    private MySqlConnection connection;
+
+    public DatabaseHandler()
+    {
+        string connectionString = "Server=localhost;Database=mydatabase;Uid=root;Pwd=password;";
+        connection = new MySqlConnection(connectionString)
+    }
+
+    public void OpenConnection()
+    {
+        connection.Open();
+    }
+
+    public void CloseConnection()
+    {
+        connection.Close();
+    }
+
+    public bool TestConnection()
+    {
+        try
+        {
+            OpenConnection();
+            CloseConnection();
+            return true;
+        }
+        catch (MySqlException ex)
+        {
+                          Console.WriteLine(ex.Message);
+            return false;
+        }
+    }
+    //TODO: correct this and end this file
+}
